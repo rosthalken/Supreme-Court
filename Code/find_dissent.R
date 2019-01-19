15f156154c2cc485ba63e528703040e43c4c6b6d
Rosamond Thalken <rosamondthalken@Rosamonds-MacBook-Pro.local>
2019-01-19 15:13:26 -0800

    First dissent files added--likely unordered

--- Code/find_dissent.R
+++ Code/find_dissent.R
@@ -3,6 +3,8 @@ library(readr)
 library(stringi)
 corpus <- "SupremeCourtCorpusFinalEncoded"
 the_dirs <- dir(corpus, pattern = ".Original")
+the_better <- dir(corpus, pattern = ".Cleaned")
+new_folder <- dir.create("DissentFiles")
 
 dissenting <- NULL
 
@@ -13,11 +15,14 @@ for(i in 1:length(the_dirs)){
   for(x in 1:length(the_files)){
     text_v <- read.table(file = file.path(corpus, the_dirs[i], the_files[x]), sep = "\r", row.names = NULL)
     result <- grep(".*dissenting\\.", text_v)
-    if(length(result) != 0){ # Hi Roz: I figured this out 30 seconds after you left :-)
-      dissenting <- c(dissenting, paste(corpus, the_dirs[i], the_files[x]))
+    if(length(result) != 0){
+      dissenting <- c(dissenting, paste(corpus, the_better[i], the_files[x]))
     }
   }
 }
 
 # Save out to the Data directory
 write.csv(dissenting, file = "Data/dissenting.csv")
+
+# now I need to use this csv file to find the file names and create a new dissent corpus
+

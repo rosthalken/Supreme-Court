c65c2b85b61a71356dedf5f3b0d5ee151855b26b
Matthew Jockers <matthew.jockers@CASJockers.local>
2019-02-11 11:43:20 -0800

    Work on ngram to wide conversion reveals problem with bigrams. . . .

--- Ngram/Code/ngram_wide.R
+++ Ngram/Code/ngram_wide.R
@@ -5,16 +5,28 @@ library(tidyr)
 library(readr)
 library(stringi)
 library(ngram)
-user_local <- "/Users/rosamondthalken/Documents/Graduate School/Thesis/Thesis Code/Ngram"
+user_local <- "/Users/matthew.jockers/Documents/Students/Thalken/thesis/Ngram"
 
 load(file.path(user_local, "Data/long_form.RData"))
 
 # mutate to create a unique primary key "ID" for each document and to create a "Feature" column that prefixes each token with its token type based on the "type" column
 long_form <- mutate(long_form, ID=paste(Author, Text_ID, sep="_"), Feature=paste(Type, Ngram, sep="_"))
 
-# Convert from long form to wide form sparse matrix
-wide_relative_df <- select(long_form, ID, Feature, Freq) %>%
-  spread(Feature, Freq, fill = 0)
+# Convert from long form to wide form sparse matrix.
+# The following code is producing Error: vector memory exhausted (limit reached?)
+# wide_relative_df <- select(long_form, ID, Feature, Freq) %>%
+#    spread(Feature, Freq, fill = 0)
+#  I have tried multiple other avenues including reshape and xtabs.  All produce same memory issue.
+#  how about dcast in data.table. . .
+library(data.table)
+df <- select(long_form, ID, Feature, Freq)
+setDT(df)
+# let's try to process a subset of 1M of the full long data. . .
+test <- dcast(df[1:1000000], ID ~ Feature, value.var = "Freq", fill = 0)
+# The next line reveals a problem with the bigrams. . . .
+test[1:10, 1:5]
+
+# stopping here. . . .
 
 save(wide_relative_df, file="Ngram/Data/wide_relative_df.RData")
 rm(wide_relative_df)

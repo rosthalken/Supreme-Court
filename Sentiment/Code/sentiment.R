444a2bf79951488273eb753b13dc844df946bf6f
Rosamond Thalken <rosamondthalken@wless-user-172023150107.temp.wsu.edu>
2019-02-15 13:05:27 -0800

    Sentiment edits

--- Sentiment/Code/sentiment.R
+++ Sentiment/Code/sentiment.R
@@ -4,26 +4,41 @@ library(dplyr)
 library(readr)
 library(stringi)
 corpus <- "SupremeCourtCorpusFinalEncoded"
+load("Data/metadata.RData")
 the_dirs <- dir(corpus, pattern = ".Cleaned")
-metadata <- NULL
+metadata_sentiment <- NULL
+year_l <- NULL
 
+for(i in 1:length(the_dirs)) {
+  the_files <- dir(file.path(corpus, the_dirs[i]))
+  for(x in 1:length(the_files)) {
+    year <- metadata$Year
+    year_df <- data.frame(the_dirs[i], the_files[x], year)
+  }
+}
 
 for(i in 1:length(the_dirs)) {
   sentiment_result <- NULL
+  author_sentiment_df <- NULL
   the_files <- dir(file.path(corpus, the_dirs[i]))
   for(x in 1:length(the_files)) {
     text_v <- get_text_as_string(file.path(corpus, the_dirs[i], the_files[x]))
     sentiment_v <- get_sentiment(text_v, method = "syuzhet")
-    sentiment_df <- data.frame(the_dirs[i], sentiment_v, stringsAsFactors = FALSE)
-    # sum_sentiment_v <- sum(sentiment_v)
-    # mean_sentiment_v <- mean(sentiment_v)
-    # sentiment_t <- data.frame(sum_sentiment_v, mean_sentiment_v)
+    year <- metadata$Year
+   # year_df <- data.frame(the_dirs[i], the_files[x], year)
+   # year <- year_df$year
+    sentiment_df <- data.frame(the_dirs[i], the_files[x], sentiment_v, year, stringsAsFactors = FALSE)
+    colnames(sentiment_df) <- c("Author", "File_Name","Sentiment", "Year")
     sentiment_result <- rbind(sentiment_result, sentiment_df)
-    
-    # Creating metadata
-    metadata <- rbind(metadata, data.frame(the_dirs[i], the_files[x], sentiment_v))
   }
   temp_name <- paste("Sentiment/RData/", the_dirs[i], ".RData", sep="")
+  mean_sentiment <- mean(sentiment_df$Sentiment)
+  #author_sentiment <- data.frame(the_dirs[i], mean(sentiment_v), stringsAsFactors = FALSE)
+  # author_sentiment_df <- data.frame(sentiment_result$Author, mean(sentiment_v), stringsAsFactors = FALSE)
+  # colnames(author_sentiment_df) <- c("Author", "Sentiment")
+  #author_sentiment_df <- rbind(author_sentiment_df, author_sentiment)
+  sentiment_result <- data.frame(sentiment_result, mean_sentiment, stringsAsFactors = FALSE)
+  #colnames(sentiment_result) <- c("Author", "File_Name", "File_Sentiment", "Author_Sentiment")
   save(sentiment_result, file=temp_name)
 }
 
@@ -36,5 +51,6 @@ for(i in 1:length(rdata_files)){
   #mean_v <- mean(sentiment_result)
   long_sentiment <- rbind(long_sentiment, sentiment_result)
 }
-save(long_form, file="Sentiment/Data/long_form.RData")
+save(long_sentiment, file="Sentiment/Data/long_sentiment.RData")
 
+sentiment_author <- select(long_sentiment, Author, Author_Sentiment)
